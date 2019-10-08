83c69833c99fea5803919c3390197f3c7eaf388a
Ethan Knop <ethanknop@gmail.com>
2019-10-08 12:42:34 -0500

    Added microphone plugin

--- App/platforms/android/platform_www/cordova_plugins.js
+++ App/platforms/android/platform_www/cordova_plugins.js
@@ -1,6 +1,16 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "cordova-plugin-audioinput.AudioInput",
+      "file": "plugins/cordova-plugin-audioinput/www/audioInputCapture.js",
+      "pluginId": "cordova-plugin-audioinput",
+      "clobbers": [
+        "audioinput"
+      ]
+    }
+  ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-audioinput": "1.0.2"
   };
 });
--- App/platforms/browser/www/cordova_plugins.js
+++ App/platforms/browser/www/cordova_plugins.js
@@ -1,9 +1,25 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-audioinput/www/audioInputCapture.js",
+        "id": "cordova-plugin-audioinput.AudioInput",
+        "pluginId": "cordova-plugin-audioinput",
+        "clobbers": [
+            "audioinput"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-audioinput/src/browser/AudioInputCaptureProxy.js",
+        "id": "cordova-plugin-audioinput.AudioInputCaptureProxy",
+        "pluginId": "cordova-plugin-audioinput",
+        "runs": true
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-audioinput": "1.0.2"
 }
 // BOTTOM OF METADATA
 });
