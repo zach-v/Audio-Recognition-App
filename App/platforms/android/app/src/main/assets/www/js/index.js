8606f1e60b11360fa0885854a3d47e43175fe5f3
Ethan Knop <ethanknop@gmail.com>
2019-10-14 14:30:56 -0500

    Updated UI
    Changed background and added animation on button press

--- App/platforms/android/app/src/main/assets/www/js/index.js
+++ App/platforms/android/app/src/main/assets/www/js/index.js
@@ -1,46 +1,37 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+var width = window.innerWidth;
+var height = window.innerHeight;
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+var elem = document.getElementById('micbutton');
+var icon = document.getElementById('speakicon');
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+icon.style.opacity = 0.9;
+//Initially place button in middle of view
+elem.style.left = (width-200)/2 + 'px';
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+elem.onclick = function() {
+    var buttonsize = 200;
+    var boxsize = 400;
+
+    var pos = 0
+    var borderRad = [50, 5];
+    var size = [buttonsize, boxsize];
+    var offset = [(width-buttonsize)/2, (width-boxsize)/2];
+
+    var id = setInterval(frame, 0.1);
+    function frame() {
+        if (pos > 1) {
+            clearInterval(id);
+        } else {
+            pos = pos + 0.01;
+            borderRadSlope = borderRad[1] - borderRad[0];
+            sizeSlope = size[1] - size[0];
+            offsetSlope = offset[1] - offset[0]
+
+            elem.style.borderRadius = (borderRad[0] + borderRadSlope*pos) + '%';
+            elem.style.width = (size[0] + sizeSlope*pos) + 'px';
+            elem.style.height = (size[0] + sizeSlope*pos) + 'px';
+            elem.style.left = (offset[0] + offsetSlope*pos) + 'px';
+        }
     }
 };
-
-app.initialize();
