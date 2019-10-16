ccd00bf66eef46d969638501255a40c651ba4ab7
Ethan Knop <36719368+EthanKnop@users.noreply.github.com>
2019-10-16 07:25:27 -0500

    Cleaned up animation code

--- App/platforms/android/app/src/main/assets/www/js/index.js
+++ App/platforms/android/app/src/main/assets/www/js/index.js
@@ -4,34 +4,46 @@ var height = window.innerHeight;
 var elem = document.getElementById('micbutton');
 var icon = document.getElementById('speakicon');
 
-icon.style.opacity = 0.9;
-//Initially place button in middle of view
+// Initially place button in middle of view
 elem.style.left = (width-200)/2 + 'px';
 
+elem.onmouseup = function() {
+    // Button/Box sizes
+    var original_size = 200;
+    var expanded_size = 360;
 
-elem.onclick = function() {
-    var buttonsize = 200;
-    var boxsize = 400;
+    // Gradients, var[0] is inital, var[1] is final
+    var expand_gradient = [original_size, expanded_size];
+    var opacity_gradient = [1, 0];
+    var border_rad_gradient = [50, 5];
+    var left_offset_gradient = [(width-original_size)/2, (width-expanded_size)/2];
+    var top_offset_gradient = [170, 80];
+    var opacity_gradient = [1, 0];
 
+    // Variable to keep track of progress in animation
     var pos = 0
-    var borderRad = [50, 5];
-    var size = [buttonsize, boxsize];
-    var offset = [(width-buttonsize)/2, (width-boxsize)/2];
 
-    var id = setInterval(frame, 0.1);
+    var id = setInterval(frame);
     function frame() {
         if (pos > 1) {
             clearInterval(id);
         } else {
-            pos = pos + 0.01;
-            borderRadSlope = borderRad[1] - borderRad[0];
-            sizeSlope = size[1] - size[0];
-            offsetSlope = offset[1] - offset[0]
-
-            elem.style.borderRadius = (borderRad[0] + borderRadSlope*pos) + '%';
-            elem.style.width = (size[0] + sizeSlope*pos) + 'px';
-            elem.style.height = (size[0] + sizeSlope*pos) + 'px';
-            elem.style.left = (offset[0] + offsetSlope*pos) + 'px';
+            // Expanding animation
+            pos = pos + 0.02;
+            // Calculate differences
+            borderRadSlope = border_rad_gradient[1] - border_rad_gradient[0];
+            sizeSlope = expand_gradient[1] - expand_gradient[0];
+            offsetSlope = left_offset_gradient[1] - left_offset_gradient[0];
+            topoffsetSlope = top_offset_gradient[1] - top_offset_gradient[0];
+            opacitySlope = opacity_gradient[1] - opacity_gradient[0];
+            
+            // Update attributes
+            elem.style.borderRadius = (border_rad_gradient[0] + borderRadSlope*pos) + '%';
+            elem.style.width = (expand_gradient[0] + sizeSlope*pos) + 'px';
+            elem.style.height = (expand_gradient[0] + sizeSlope*pos) + 'px';
+            elem.style.left = (left_offset_gradient[0] + offsetSlope*pos) + 'px';
+            elem.style.marginTop =  (top_offset_gradient[0] + topoffsetSlope*pos) + 'px';
+            icon.style.opacity = (opacity_gradient[1] + pos*opacitySlope) + '';
         }
     }
 };
