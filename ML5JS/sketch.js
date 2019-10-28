71f4dec9d6b7bddce666eafbd339e6a44a7f0f72
Zachary Vanscoit <zachary816@gmail.com>
2019-10-28 15:46:04 -0500

    updated ml5js display
    Added display on webpage and words to say

--- ML5JS/sketch.js
+++ ML5JS/sketch.js
@@ -2,34 +2,46 @@
 console.log('ml5 version:', ml5.version);
 
 const options = {probabilityThreshold: 0.7};
-const classifier = ml5.soundClassifier('SpeechCommands18w', options, modelReady);
+let classifier;
+let resultP;
 /*
 This is used for a custom model made with Google's Teachable Machine
 const classifier = ml5.soundClassifier('path/to/model.json', modelReady);
 */
 
+function preload() {
+	classifier = ml5.soundClassifier('SpeechCommands18w', options, modelReady);
+}
 
 // P5 init function
 function setup() {
 	createCanvas(400,400);
+	resultP = createP('waiting...');
+	resultP.style('font-size','32pt');
 }
 
 // P5 draw loop
 function draw() {
-	background(100);
+	text('Try saying these words!\n "zero" to "nine", "up", "down", "left",\n "right", "go", "stop", "yes", "no"', 0, height / 2);
+	noDraw(true);
 }
 
 // method we reference to classify sound
 function modelReady() {
+	console.log('Model is ready to go.');
 	classifier.classify(gotResult);
 }
 
 function gotResult(error, result) {
 	if (error) {
+		console.log('Something happened...');
 		console.log(error);
 		return;
 	}
 	
 	// log result
-	console.log(result);
+	console.log(result[0].label, result[0].confidence);
+
+	// display result in html
+	resultP.html(`${result[0].label} : ${result[0].confidence}`);
 }
