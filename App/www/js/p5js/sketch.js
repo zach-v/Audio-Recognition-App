5bdcf34aca345a0f1cb46518beb9b3f3ccf6e78e
EthanKnop <ethanknop@me.com>
2019-11-21 11:45:41 -0600

    Added visualizer to index

--- App/www/js/p5js/sketch.js
+++ App/www/js/p5js/sketch.js
@@ -1,52 +1,126 @@
-let mic, fft, displayArray, scale;
+var mic, soundFile;
+
+// Visualizer variables
+var amplitude;
+var prevLevels = new Array(100);
+var incre = 0;
+
+// ML variables
+const options = {probabilityThreshold: 0.7};
+let classifier;
+let resultP;
+var textField = document.getElementById('outputtext');
+
+function preload() {
+	classifier = ml5.soundClassifier('SpeechCommands18w', options, modelReady);
+}
+
 
 function setup() {
-  var canvas = createCanvas(400, 400);
-  noFill();
-  canvas.parent('visualizer');
-  frameRate(30);
-  
-  displayArray = new Array(20);
-  
-  scale = width / 10;
+  c = createCanvas(window.innerWidth, window.innerHeight);
+  c.parent(document.getElementById('visualizer'));
+
+  noStroke();
+  noLoop();
+
+  rectMode(CENTER);
+  colorMode(HSB);
 
   mic = new p5.AudioIn();
   mic.start();
-  fft = new p5.FFT(0.8, 1024);
-  fft.setInput(mic);
+
+
+  amplitude = new p5.Amplitude();
+  amplitude.setInput(mic);
+  amplitude.smooth(0.8);
+}
+
+function runNow() {
+  console.log("Running visualizer.");
+  loop();
+}
+
+function resizeAudioCanvas() {
+  resizeCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
+  clear();
+  fill(255, 10);
 
-  let spectrum = fft.analyze();
+  if (getAudioContext().state !== 'running')
+    getAudioContext().resume();
 
-  getAudioContext().resume();
-  
-  let z = 0;
+  var level = amplitude.getLevel();
 
-  for (j = 0; j < displayArray.length; j++) {
-    if (j == 0)
-      displayArray[0] = spectrum;
-    else {
-      displayArray[j] = reduce(displayArray[j - 1]);
-    }
-    
-    stroke(map(j, 0, displayArray.length, 0, 200));
-    
-    beginShape();
-    for (i = 2; i < displayArray[j].length; i++) {
-      vertex(map(i, 0, displayArray.length, 0, width / 14),
-             map(displayArray[j][i], 0, 255, (height / 2) + z, 0));
-    }
-    endShape();
-    z += 10;
+  // rectangle variables
+  var spacing = 10;
+  var w = width/ (prevLevels.length * spacing);
+
+  var minHeight = 2;
+  var roundness = 20;
+
+  prevLevels.push(level);
+  prevLevels.splice(0, 1);
+
+  for (var i = 0; i < prevLevels.length; i++) {
+
+    var x = map(i, prevLevels.length, 0, width/2, width);
+    var h = map(prevLevels[i], 0, 0.2, minHeight, height);
+
+    var alphaValue = logMap(i, 0, prevLevels.length, 1, 250);
+
+    var hueValue = map(h, minHeight, height, 200, 255);
+
+    incre += 0.005;
+
+    fill(incre % 360, 255, 255, alphaValue);
+
+    if (incre > 360)
+    incre = 0;
+
+    rect(x, height/2, w, h);
+    rect(width - x, height/2, w, h);
   }
+} //End of draw()
+
+function logMap(val, inMin, inMax, outMin, outMax) {
+
+  for (var i = 0; i < arguments.length; i++) {
+    if (arguments[i] === 0) {
+      arguments[i] = 0.0000000000000001;
+    }
+  }
+
+  var minv = Math.log(outMin);
+  var maxv = Math.log(outMax);
+
+  var numerator = maxv - minv;
+  var denom = inMax - inMin;
+
+  if (denom === 0) {
+    denom = 0.000000000001;
+  }
+
+  var scale = numerator / denom;
+
+  return Math.exp(minv + scale*(val-inMin));
 }
 
-function reduce(array) {
-  let sender = new Array(array.length);
-  for (i = 0; i < array.length; i++) {
-    sender[i] = array[i] * 0.9;
-  }
-  return sender;
+// method we reference to classify sound
+function modelReady() {
+	console.log('Model is ready to go.');
+	classifier.classify(gotResult);
+}
+
+function gotResult(error, result) {
+	if (error) {
+		console.log('Something happened...');
+		console.log(error);
+		return;
+	}
+
+	// Update text field
+	textField.innerText = `${result[0].label} \n ${Number((result[0].confidence*100).toFixed(1))}%`;
+	textField.style.bottom = 30 + 'px';
 }
